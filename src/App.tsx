064a4a70a3850ad7b4601f01553c13383089cf69
Naman Ajmera <cool.naman.ajmera@gmail.com>
2024-08-12 15:27:06 +0530

    Added: Install the required packages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline">
       Hello E-Commerce.
+      <h1>Test</h1>
     </div>
   );
 }
